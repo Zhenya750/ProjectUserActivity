bad3b2562ed22ba8917ab0e9cfb9bc6dc3973330
zhenya750 <zhenya750bus@mail.ru>
2021-08-18 20:11:00 +0300

    added axes description

--- ProjectUserActivity/ClientApp/src/components/Histogram.js
+++ ProjectUserActivity/ClientApp/src/components/Histogram.js
@@ -5,18 +5,26 @@ import { Chart } from 'react-google-charts';
 export class Histogram extends React.Component {
     render() {
         const options = {
-            title: 'Users total days',
+            title: 'Users activity distribution',
             legend: { position: 'none' },
             colors: [ '#4A9DFF'],
             backgroundColor: 'white',
+            hAxis: {
+                title: 'Days',
+                minValue: 0,
+            },
+            vAxis: {
+                title: 'Users',
+                minValue: 0,
+            }
         };
 
         return (
             <Container className="histogram white-shadow">
                 <Chart
-                chartType="Histogram"
-                data={this.props.data}
-                options={options} />
+                    chartType="Histogram"
+                    data={this.props.data}
+                    options={options} />
             </Container>
         );
     }
